9e2dbd86d4fa96479dd924c44cd5a3b55eaad9ce
Marek Belski <marek.belski@gmail.com>
2025-05-13 10:07:42 -0700

    Adds score UI.

---
+++ scenes/game_scene/score_ui.gd
@@ -0,0 +1,11 @@
+@tool
+extends HBoxContainer
+
+@export var score : int = 0 :
+	set(value):
+		score = value
+		if is_inside_tree():
+			label.text = "%08d" % score
+		
+
+@onready var label : Label = %Label
