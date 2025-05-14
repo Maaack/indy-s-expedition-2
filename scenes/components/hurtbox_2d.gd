a74d814db94afd23583682346b458a7516442ceb
Marek Belski <marek.belski@gmail.com>
2025-05-14 16:48:33 -0700

    Adds knockback to PC. Swaps hurtbox/hitbox monitoring.

--- scenes/components/hurtbox_2d.gd
+++ scenes/components/hurtbox_2d.gd
@@ -29,25 +29,3 @@ func damage(incoming_damage : float):
 	for component in _components:
 		if component.has_method(&"damage"):
 			component.damage(final_damage)
-
-func _on_area_entered(area):
-	if area is Hitbox2D:
-		damage(area.damage)
-	if area is AreaProjectile:
-		var final_damage = _get_final_damage(area.shot_data.damage)
-		damage(area.shot_data.damage)
-		if _hurt_audio_player_2d != null:
-			_hurt_audio_player_2d.play()
-		area.queue_free()
-	if area is TriggerArea2D:
-		area.trigger()
-		
-func _on_body_entered(_body):
-	pass
-
-func _ready():
-	if _type & AREA_FLAG:
-		connect("area_entered", _on_area_entered)
-	if _type & BODY_FLAG:
-		connect("body_entered", _on_body_entered)
-		
