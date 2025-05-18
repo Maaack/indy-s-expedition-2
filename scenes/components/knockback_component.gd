23284d52b626f9d2f11735e89389df3b1cc58ce7
Marek Belski <marek.belski@gmail.com>
2025-05-17 21:32:43 -0700

    Have whip pull treasures toward whip contact position.

--- scenes/components/knockback_component.gd
+++ scenes/components/knockback_component.gd
@@ -2,7 +2,8 @@ class_name KnockbackComponent
 extends ComponentBase
 
 @export var character_2D : CharacterBody2D
+@export var modifier : float = 1.0
 
 func knockback(knockback_position : Vector2, force : float) -> void:
 	var knockback_direction := character_2D.global_position - knockback_position
-	character_2D.velocity += knockback_direction.normalized() * force
+	character_2D.velocity += knockback_direction.normalized() * force * modifier
--- scenes/pickups/pickup.gd
+++ scenes/pickups/pickup.gd
@@ -15,16 +15,16 @@ var is_picked_up : bool = false
 	set(value):
 		is_draggable = value
 		set_physics_process(is_draggable)
+@export var friction : float = 300
 
 var is_dragged : bool = false
-var friction : float = 300
 
 func _physics_process(delta):
 	if not is_dragged:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-		if get_position_delta().length_squared() < 1:
-			velocity = Vector2.ZERO
 	move_and_slide()
+	if get_position_delta().length_squared() < 1 and velocity < Vector2.ONE * 0.1:
+		velocity = Vector2.ZERO
 
 func _process_pickup():
 	emit_signal("picked_up")
